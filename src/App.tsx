eb56eabd54129995d0dfbc49941cafadcfd033fa
Oogway <animagig@gmail.com>
2026-05-12 22:31:28 +0900

    movieapp

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Detail from "./routes/Detail";
 function App() { 
     
     return (
-        <Router basename="/ts-react-for-beginners">
+        <Router basename="/movieapp">
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="movie/:id" element={<Detail />} />
